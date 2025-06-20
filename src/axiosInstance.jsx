1f6924f0bf1ecbca8eb2b4ce917e5943eef22c89
Dilshad <dilshadalicp28@gmail.com>
2025-06-20 15:03:50 +0530

    update base url

--- src/axiosInstance.jsx
+++ src/axiosInstance.jsx
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL:"https://eduflow-backend-2-kjdp.onrender.com/",  
+  baseURL:"https://eduflow-backend-4-mv0y.onrender.com/",  
 });
 
 axiosInstance.interceptors.request.use((config) => {
